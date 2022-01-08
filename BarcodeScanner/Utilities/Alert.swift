db4638ec0a88460485d391836500dc789b77ea04
Yuri Ershov <shtorm0606@gmail.com>
2022-01-08 20:50:46 +0300

    A little cleanup

--- BarcodeScanner/Utilities/Alert.swift
+++ BarcodeScanner/Utilities/Alert.swift
@@ -9,16 +9,16 @@ import SwiftUI
 
 struct AlertItem: Identifiable {
     let id = UUID()
-    let title: String
-    let message: String
+    let title: Text
+    let message: Text
     let dismissButton: Alert.Button
 }
 
 struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
-                                              message: "Something is wrong with the Camera.",
+    static let invalidDeviceInput = AlertItem(title: Text("Invalid Device Input"),
+                                              message: Text("Something is wrong with the Camera."),
                                               dismissButton: .default(Text("Ok")))
-    static let invalidScannedType = AlertItem(title: "Invalid Scanned Type",
-                                              message: "The value scanned is not valid.",
+    static let invalidScannedType = AlertItem(title: Text("Invalid Scanned Type"),
+                                              message: Text("The value scanned is not valid."),
                                               dismissButton: .default(Text("Ok")))
 }
